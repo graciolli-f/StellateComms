b7c7cf41d4a058701583b6b430ab15b29773ac1f
Fernanda Vieira <fgvieira@Vieiras-MacBook-Air.local>
2022-04-09 23:54:45 -0400

    wrapper is 70vw but caitlin flashes before card opens

--- main.js
+++ main.js
@@ -1,28 +1,51 @@
 $(document).ready(function(){
 
-    $('.bio-arrow').click(function(){
+    $('.bio-arrow').click(async function(){
         var card = $(this).parent().parent();
         var otherCards = $('.member-card-wrapper').not(card);
+        var grid = $(this).closest('.team-grid');
 
-        otherCards.css ({
-            'display' : 'none'
+        // $('body').css({'height' : '100vh'});
+
+        otherCards.animate({
+            opacity: '0',
+          }, 500, function(noDisplay){
+            $(this).css('display', 'none');
         })
 
+            $(this).closest('.member-card-wrapper').delay(700).css ({
+                'width' : '70vw'
+            });
+            $(this).closest('.member-card-inner').delay(700).css({
+            'width' : '25%'
+            });
+        // }
+        // $when(noDisplay).done.member-card-wrapper').css({
+        //     'width' : '70vw',
+        //     // 'transition' : '.4s ease-in-out'
+        // });
+        // $('.member-card-inner').css({
+        //     'width' : '25%'
+        // });
+
+        // otherCards.css({'display' : 'hidden'});
+
         card.css ({
-            '-webkit-transform' : 'scale(150%)',
-            '-moz-transform'    : 'scale(150%)',
-            '-ms-transform'     : 'scale(150%)',
-            '-o-transform'      : 'scale(150%)',
-            'transform'         : 'scale(150%)',
+            '-webkit-transform' : 'scale(125%)',
+            '-moz-transform'    : 'scale(125%)',
+            '-ms-transform'     : 'scale(125%)',
+            '-o-transform'      : 'scale(125%)',
+            'transform'         : 'scale(125%)',
             'transform-origin'  : 'left',
-            'margin-top'        : '100px',
+            'margin-top'        : '1.2em',
+            'grid-area'         : '1',
             'transition'        : '0.6s ease-in-out',
-            'transition-delay'  : '70ms'
         });
-    
 
-        // card.animate({width: '100%'}, "slow");
+        grid.addClass('team-grid-bio-open');
+        grid.removeClass('team-grid');
 
+        
 
     //   $('.team-grid').addClass('team-grid-bio-open');
     //   $('.team-grid').removeClass('team-grid');
